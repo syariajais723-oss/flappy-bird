0e2aeebe39467b4794931a5fdabf7bd12793de19
syariajais723-oss <syariajais723@gmail.com>
2026-01-22 18:55:00 +0700

    Add files via upload

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;          // Rigidbody player
+    public float jumpForce;  // besar loncatan
+    public int score = 0;    // skor pemain
+
+    public Text scoreTxt;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();  // ambil komponen Rigidbody2D
+    }
+
+    void Update()
+    {
+        scoreTxt.text = "" + score;
+
+
+        if (Input.GetMouseButtonDown(0))  // klik kiri / tap layar
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("poin"))
+        {
+            score++;
+            Debug.Log("Score: " + score);
+        }
+
+        if(collision.gameObject.tag =="pipa")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
